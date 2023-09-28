b20f8329ad819ca004a108357e08858e34a479b6
Dan Carter <djcarter85@gmail.com>
2023-09-28 17:31:04 +0100

    Use a custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,md}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
